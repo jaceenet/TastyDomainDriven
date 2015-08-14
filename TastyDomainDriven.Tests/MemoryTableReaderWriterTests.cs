01a4e974af989cda8021a71ccc6e873dc12b02fa
jaceenet <jacee.net@gmail.com>
2015-08-14 16:46:40 +0200

    added GetHashCode in new implementation MemoryHashTableWriter instead, to keep supporting old codebase

--- TastyDomainDriven.Tests/MemoryTableReaderWriterTests.cs
+++ TastyDomainDriven.Tests/MemoryTableReaderWriterTests.cs
@@ -4,18 +4,42 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace TastyDomainDriven.Tests
 {
     public class MemoryTableReaderWriterTests
     {
-	    public void WriteOnce()
+		[Fact]
+		public void AddAndUpdateTest()
 	    {
 			var writer = new TastyDomainDriven.Projections.MemoryTableWriter<MyItem>();
-	    }
-    }
 
-	public class MyItem
+		    Guid myid = Guid.NewGuid();
+
+		    writer.AddOrUpdate(new MyItem("john", myid), x =>
+		    {
+			    x.Titel = "Mr";
+		    });
+
+			writer.AddOrUpdate(new MyItem("john2", myid), x =>
+			{
+				x.Titel = "Mr 2";
+			});
+
+			writer.AddOrUpdate(new MyItem("john2", myid), x =>
+			{
+				x.Titel = "Mr renamed";
+			});
+
+			Assert.Equal(2, writer.GetAll().Result.Count);
+			Assert.Equal("Mr", writer.Get(new MyItem("john", myid)).Result.Titel);
+			Assert.Equal("Mr renamed", writer.Get(new MyItem("john2", myid)).Result.Titel);
+			Assert.Null(writer.Get(new MyItem("joHn2", myid)).Result);
+		}		
+	}
+
+	public class MyItem : ISingleKey
 	{
 		private string _name;
 		private readonly Guid _accountId;
@@ -30,7 +54,7 @@ namespace TastyDomainDriven.Tests
 		public MyItem(string name, Guid accountId)
 		{
 			this._name= name;
-			_accountId = accountId;
+			this._accountId = accountId;
 		}
 
 		public override bool Equals(object obj)
@@ -54,9 +78,14 @@ namespace TastyDomainDriven.Tests
 			}
 		}
 
+		public string GetIndexKey()
+		{
+			return string.Concat(this.Name, this.AccountId);
+		}
+
 		public override string ToString()
 		{
-			return this._name.ToString();
+			return this._accountId.ToString("N") + " - " + this._name.ToString();
 		}
 	}
 }
