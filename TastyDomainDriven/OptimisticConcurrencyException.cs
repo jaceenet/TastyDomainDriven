700cc2625b0902721d97673a2b154b63e4d5ac5f
jaceenet <jacee.net@gmail.com>
2016-06-14 00:19:33 +0200

    more information on OptimisticConcurrencyException

--- TastyDomainDriven/OptimisticConcurrencyException.cs
+++ TastyDomainDriven/OptimisticConcurrencyException.cs
@@ -11,22 +11,24 @@ namespace TastyDomainDriven
         public long ExpectedVersion { get; private set; }
         public IIdentity Id { get; private set; }
         public IList<IEvent> ActualEvents { get; private set; }
+        public IList<IEvent> ConflictingEvents { get; private set; }
 
         OptimisticConcurrencyException(string message, long actualVersion, long expectedVersion, IIdentity id,
-            IList<IEvent> serverEvents)
+            IList<IEvent> serverEvents, IList<IEvent> conflictingEvents)
             : base(message)
         {
             this.ActualVersion = actualVersion;
             this.ExpectedVersion = expectedVersion;
             this.Id = id;
             this.ActualEvents = serverEvents;
+            this.ConflictingEvents = conflictingEvents;
         }
-
+        
         public static OptimisticConcurrencyException Create(long actual, long expected, IIdentity id,
-            IList<IEvent> serverEvents)
+            IList<IEvent> serverEvents, IList<IEvent> conflictEvents)
         {
             var message = string.Format("Expected v{0} but found v{1} in stream '{2}'", expected, actual, id);
-            return new OptimisticConcurrencyException(message, actual, expected, id, serverEvents);
+            return new OptimisticConcurrencyException(message, actual, expected, id, serverEvents, conflictEvents);
         }
 
         protected OptimisticConcurrencyException(
