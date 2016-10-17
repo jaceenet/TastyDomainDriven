3350776a203dac253713f0e7aec6b38860285f09
jaceenet <jacee.net@gmail.com>
2016-10-17 20:38:43 +0200

    copy from non async

--- TastyDomainDriven/AsyncImpl/AsyncAppendOnlyExtension.cs
+++ TastyDomainDriven/AsyncImpl/AsyncAppendOnlyExtension.cs
@@ -23,5 +23,24 @@ namespace TastyDomainDriven.AsyncImpl
             }
 
         }
+
+        public static async Task CopyAppender(this IAppendOnlyAsync to, IAppendOnlyStore @from)
+        {
+            var records = @from.ReadRecords(0, int.MaxValue);
+
+            var versions = new Dictionary<string, int>();
+
+            foreach (var record in records)
+            {
+                if (!versions.ContainsKey(record.Name))
+                {
+                    versions[record.Name] = 0;
+                }
+
+                await to.Append(record.Name, record.Data, versions[record.Name]);
+                versions[record.Name]++;
+            }
+
+        }
     }
 }
